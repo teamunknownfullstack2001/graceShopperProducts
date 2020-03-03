348c0b636ee276298ae2189a31d48bb174cdc7e2
ianxinAngland <tianxinangland@Tianxins-MBP.fios-router.home>
2020-03-02 22:10:08 -0500

    wrap React app in an stripe Elements provider

--- client/index.js
+++ client/index.js
@@ -5,14 +5,21 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
+import {Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
+// import CheckoutForm from './CheckoutForm';
 
 // establishes socket connection
 import './socket'
 
+const stripePromise = loadStripe('pk_test_pPRna1khntUpsR5cAJIdpJQ500hjN9lKlz')
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
     </Router>
   </Provider>,
   document.getElementById('app')
