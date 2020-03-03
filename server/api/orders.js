ebe783a6ac7937c440531475b92408f273d54e07
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2020-03-02 21:34:11 -0500

    I created orders api. I wrote up a route to serve all orders and one to serve a singe order. I mounted it in server/api/index.js

---
+++ server/api/orders.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const {Order} = require('../db/models/order')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const allOrders = await Order.findAll()
+    res.json(allOrders)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const singleOrder = await Order.findById(req.params.id)
+    res.json(singleOrder)
+  } catch (error) {
+    next(error)
+  }
+})
