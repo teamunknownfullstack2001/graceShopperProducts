e1a6718e2a9e7d7373e64a38c5d35d36853627c3
ianxinAngland <tianxinangland@Tianxins-MacBook-Pro.local>
2020-03-10 12:56:07 -0400

    form now working

--- client/components/AddressForm.js
+++ client/components/AddressForm.js
@@ -26,7 +26,8 @@ export default class AddressForm extends React.Component {
                 type="text"
                 className="form-control "
                 name="name"
-                placeholder=""
+                placeholder="your user name"
+                id="validationCustomUsername"
                 value={state.name}
                 onChange={handleChange}
                 required
@@ -64,6 +65,7 @@ export default class AddressForm extends React.Component {
                 onChange={handleChange}
                 required
               />
+              <div className="valid-feedback">Looks good!</div>
               <div className="invalid-feedback">
                 Please enter your shipping address.
               </div>
--- client/components/EditUserForm.js
+++ client/components/EditUserForm.js
@@ -23,11 +23,7 @@ class DisEditUserForm extends React.Component {
         : {...props.initialValues}
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    // this.initialValsFromProps = this.initialValsFromProps.bind(this)
   }
-  // initialValsFromProps(field, initialValues) {
-  //   return initialValues ? initialValues[field] : ''
-  // }
 
   handleChange(event) {
     console.log('typing', event.target.name, event.target.value)
@@ -49,23 +45,56 @@ class DisEditUserForm extends React.Component {
   render() {
     return (
       <div className="col-md-8 order-md-1">
-        <form onSubmit={this.handleSubmit} className="needs-validation">
-          <label className="mb-3" htmlFor="userName">
-            Name:
-          </label>
-          <input
-            className="form-control"
-            type="text"
-            name="userName"
-            value={this.state.userName}
-            onChange={this.handleChange}
-          />
+        <form
+          onSubmit={this.handleSubmit}
+          className="needs-validation"
+          noValidate
+        >
+          <div className="d-flex flex-column">
+            <div className="mb-3">
+              <label>Name</label>
+              <input
+                type="text"
+                className="form-control "
+                name="name"
+                placeholder=""
+                // value={this.props.state.name}
+                onChange={this.handleChange}
+                required
+              />
+              <div className="invalid-feedback">Name is required.</div>
+            </div>
+          </div>
+
+          <button className="btn btn-primary btn-lg btn-block" type="submit">
+            Continue to checkout
+          </button>
+        </form>
+
+        <form
+          onSubmit={this.handleSubmit}
+          className="needs-validation"
+          noValidate
+        >
+          <div className="mb-3">
+            <label>Name:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="userName"
+              required
+              id="userName"
+              value={this.state.userName}
+              onChange={this.handleChange}
+            />
+            <div className="invalid-feedback">Name is required.</div>
+          </div>
           <label className="mb-3" htmlFor="email">
             Email:
           </label>
           <input
             className="form-control"
-            type="text"
+            type="email"
             name="email"
             value={this.state.email}
             onChange={this.handleChange}
@@ -77,6 +106,7 @@ class DisEditUserForm extends React.Component {
             className="form-control"
             type="text"
             name="address"
+            required
             onChange={this.handleChange}
             value={this.state.address}
           />
@@ -87,6 +117,7 @@ class DisEditUserForm extends React.Component {
             className="form-control"
             type="text"
             name="zip"
+            required
             onChange={this.handleChange}
             value={this.state.zip}
           />
@@ -97,6 +128,7 @@ class DisEditUserForm extends React.Component {
             className="form-control"
             type="text"
             name="phone"
+            required
             onChange={this.handleChange}
             value={this.state.phone}
           />
