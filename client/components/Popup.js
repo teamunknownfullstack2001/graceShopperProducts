5ce744b739a548af86d6e2cb4581cc47f83abc97
mczernyk <mczernyk@gmail.com>
2020-03-11 03:28:15 -0400

    popup

--- client/components/Popup.js
+++ client/components/Popup.js
@@ -19,15 +19,20 @@ export default class Popup extends Component {
     console.log('popup')
     return (
       <div className="modal_content">
-        <p>
-          <i>Success!</i>
-        </p>
-        <div>
+        <div className="popup">
+          <p>
+            <i>Success!</i>
+          </p>
+        </div>
+
+        <div className="popup">
           <p>
             You've added a new friend to your cart... Nice! Meeting people is
             hard - Why bother making friends the old fashioned way when you can
             just buy one from us?
           </p>
+        </div>
+        <div className="popup">
           <Link to="/products" style={{textDecoration: 'none', color: 'black'}}>
             <Button>Browse Friends</Button>
           </Link>
